b18c0e563352a32d52c65ac2d4fb12847170266f
Blaine Hoon <bhoon07@gmail.com>
2020-08-08 13:46:54 -0500

    first updates

---
+++ script.js
@@ -0,0 +1,5 @@
+var $title=document.querySelector("#title");
+var $welcomeMessage=document.querySelector("#welcomeMessage");
+var $Highscores=document.querySelector("#Highscores");
+var $timer=document.querySelector("#timer");
+var $button=document.querySelector("#button");
